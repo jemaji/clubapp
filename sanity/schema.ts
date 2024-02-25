1b40c0b92ba65a731ed5645d2dd427ebc9de095c
Francisco Javier Ponce Rosales <franciscojavier.poncerosales@gmail.com>
2024-02-25 20:41:14 +0100

    add: entities into studio

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,79 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    {
+      type: 'document',
+      name: 'urlImage',
+      fields: [
+        {
+          type: 'string',
+          name: 'title',
+          title: 'name',
+        },
+        {
+          type: 'url',
+          name: 'image',
+          title: 'Image',
+        },
+      ],
+    },
+    {
+      type: 'document',
+      name: 'post',
+      fields: [
+        {
+          type: 'string',
+          name: 'title',
+          title: 'Title',
+        },
+        {
+          type: 'text',
+          name: 'body',
+          title: 'Body',
+        },
+        {
+          type: 'urlImage',
+          name: 'mainImage',
+          title: 'Main image',
+        },
+      ],
+    },
+    {
+      type: 'document',
+      name: 'landingPage',
+      fields: [
+        {
+          type: 'string',
+          name: 'title',
+          title: 'Title',
+        },
+        {
+          type: 'text',
+          name: 'body',
+          title: 'Body',
+        },
+        {
+          type: 'urlImage',
+          name: 'mainImage',
+          title: 'Main image',
+        },
+        {
+          type: 'array',
+          name: 'urlImages',
+          title: 'Images',
+          of: [
+            {
+              type: 'urlImage',
+            },
+          ],
+        },
+        {
+          type: 'string',
+          name: 'subtitle',
+          title: 'Subtitle',
+        },
+      ],
+    },
+  ],
 }
